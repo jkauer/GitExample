68b1d46a1bc71af2a6e1ec4b1982ed52128e512a
Squid <thecommando1262@gmail.com>
2016-01-13 16:09:51 -0700

    Ehh.

--- Example.java
+++ Example.java
@@ -2,6 +2,11 @@ public class Example
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hi my name is Jeff");
+<<<<<<< HEAD
+		System.out.println("Hi my name is Mason");
+		System.out.println("Hi my name is Kanon");
+=======
+		System.out.println("Hi");
+>>>>>>> parent of 9dd8765... Added my name is Jeff
 	}
 }
